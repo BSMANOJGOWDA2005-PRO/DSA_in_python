322792079ba24687002c992ded2d2d9230f21a9a
B S Manoj Gowda <manojgowdamanu005@gmail.com>
2026-07-14 19:55:53 +0530

    Add files via upload

---
+++ Binary_Search/binary_search_floor.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+# Create BST
+root = Node(8)
+root.left = Node(3)
+root.right = Node(10)
+
+root.left.left = Node(1)
+root.left.right = Node(6)
+
+root.left.right.left = Node(4)
+root.left.right.right = Node(7)
+
+root.right.right = Node(14)
+root.right.right.left = Node(13)
+
+
+# Find Floor
+def find_floor(root, inp):
+    floor = -1
+
+    while root is not None:
+        if root.val == inp:
+            return root.val
+        elif root.val < inp:
+            floor = root.val
+            root = root.right
+        else:
+            root = root.left
+
+    return floor
+
+
+inp = 12
+print("Floor:", find_floor(root, inp))
